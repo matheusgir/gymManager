e138e6e0a34394ee88795e0250b410fe846d2305
matheusgir <girardi.matheus@hotmail.com>
2020-06-30 20:36:46 -0300

    JSON and Date

--- instructors.js
+++ instructors.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const data = require("./data.json")
+
+// CREATE
+
 exports.post = function(req, res) {
   
     // if(req.body.name == "") {
@@ -7,9 +12,22 @@ exports.post = function(req, res) {
     const keys = Object.keys(req.body)
 
     for(key of keys) {
-        if(req.body[key]=="")
+        if(req.body[key]=="") {
             return res.send('Please, fill all fields!')
+        }
     }
 
-   return res.send(req.body)
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+
+    //[]
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
+
+  // return res.send(req.body)
 }
